7c5e482859f3628e5cc2e593bb2b4e7b32f7a346
Ocheretenko Oleksii <b8mission@gmail.com>
2020-06-03 23:34:48 +0300

    navigation template  added; 404 temlate; globals-> new method; database config; static->main.css added; includes update

--- classes/Globals.php
+++ classes/Globals.php
@@ -12,4 +12,31 @@ class Globals
 
         require $path;
     }
+
+    public static $pdo;
+
+    public static function Init()
+    {
+        try {
+            self::$pdo =
+                new PDO('mysql:host='. DB_HOST .';dbname=' . DB_NAME,
+                    DB_USER,
+                    DB_PASSWORD);
+        }
+        catch (Exception $exception)
+        {
+            echo 'db error';
+        }
+    }
+
+    public static function AskCss($name)
+    {
+        if (strpos($name,'.css') === false)
+            $name .= '.css';
+
+        $name = '/static/styles/' .  $name;
+        echo '<link rel="stylesheet" href="' .$name. '">';
+    }
 }
+
+Globals::Init();
--- includes.php
+++ includes.php
@@ -1,5 +1,6 @@
 <?php
 
 require 'config/main_config.php';
+require 'config/database_config.php';
 require 'classes/Router.php';
 require 'classes/Globals.php';
